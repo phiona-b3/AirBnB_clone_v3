44622ee09976b532b834127264cc827709c6154d
phiona-b3 <phiona030303@gmail.com>
2022-11-20 09:34:47 -0800

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,30 @@
 """checking on the status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+from models.user import User
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """return status ok"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def count_classes():
+    """retrieves the number of each objects by type"""
+    classes = [Amenity, City, Place, Review, State, User]
+    show = ["amenities", "cities", "places", "reviews", "states", "users"]
+
+    dict_ahow = {}
+
+    for clas in range(len(classes)):
+        dict_show[show[clas]] = storage.count(classes[clas])
+
+    return jsonify(dict_show)
