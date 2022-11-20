aa8b29750fac25bc4e285121d19f575ec1d2872a
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-20 17:50:20 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,4 +2,4 @@
 """creating a variable app_views an instance of Blueprint"""
 from flask import Blueprint
 
-app_views = Blueprint('app.views', __name__, url_prefix="/api/v1")
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
