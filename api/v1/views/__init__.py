e3a465db5e01a1f11379041621ff54b2c5b94543
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-22 21:47:40 +0300

    Task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """creating a variable app_views an instance of Blueprint"""
 from flask import Blueprint
-from api.v1/views/index import *
+from api.v1.views import index
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
