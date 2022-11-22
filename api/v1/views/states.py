228a73f162c2105f4dda0f3c1e8bb8d11e6488ae
Phiona Beatrice Ndemaru <102053232+phiona-b3@users.noreply.github.com>
2022-11-22 21:15:33 +0300

    Task

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -73,7 +73,7 @@ def update_state(state_id=None):
     data = request.get_json()
 
     for key, value in data.items():
-        if key != "id" or key is not "created_at" or key is not "updated_at":
+        if key != "id" or key != "created_at" or key != "updated_at":
             setattr(state, key, value)
 
     storage.save()
