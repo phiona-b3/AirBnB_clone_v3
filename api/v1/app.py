313ff46027f2c4c2b9b8a6c76e09a8d23a663485
phiona-b3 <phiona030303@gmail.com>
2022-11-22 11:55:10 -0800

    Task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins":"0.0.0.0"}})
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
